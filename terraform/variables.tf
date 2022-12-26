e63bd4a05c58b33786f284292af0509d821bd13a
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-12-26 16:54:31 -0300

    feat: initial vars

---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "project" {
+  type        = string
+  description = "GCP project id"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP project region"
+}
+
+variable "zone" {
+  type        = string
+  description = "GCP project zone"
+}
+
+variable "nodes_map" {
+  type        = list(map(string))
+  description = "List of key-par objects that uses the following structure: {'name' = 'my-name', 'is-master' = false|true } "
+}
+
+variable "gce_username" {
+  type        = string
+  description = "Username used to login via ssh"
+}
+
+variable "gce_ssh_key_file_path" {
+  type        = string
+  description = "Path to a id_rsa.pub file to be used to validate ssh authentication"
+}
+
+variable "master_script" {
+  type        = string
+  description = "Script used to setup the master node"
+}
+
+variable "worker_script" {
+  type        = string
+  description = "Scritp used to setup the workers node"
+}
