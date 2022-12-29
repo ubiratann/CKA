bbe9fb4b60623d1b383d3ae76c9751919e92beb3
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-12-29 15:51:12 -0300

    fix: kubeadm command

--- terraform/scripts/master.sh
+++ terraform/scripts/master.sh
@@ -72,7 +72,7 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-sudo kubeadmn init
+sudo kubeadm init
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
